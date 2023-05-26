986a8c7ab51f5e340f4de3aa691b329e04056d7d
abhin raj <abhinrajmanu@gmail.com>
2023-05-26 08:49:51 +0530

    no changes

---
+++ aj.py
@@ -0,0 +1,39 @@
+
+def display(a,b):
+    c=a+b
+    print(c)
+# a=int(input("number1:"))
+# b=int(input("number2:"))
+# display(a,b)
+
+
+def subtract(a,b):
+    sub=a-b
+    print("result",sub)
+
+
+def Multiply(a,b):
+    mult=a*b
+    print("reslut:",mult)
+
+
+def divide(a,b):
+    divide=a/b
+    print("result;",divide)
+
+
+text = "hello"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sample.py
@@ -0,0 +1,23 @@
+import aj
+
+print("welcome")
+a=int(input("number1:"))
+b=int(input("number2:"))
+choice=int(input("Your choice:1.display,2.subtract"))
+
+if choice==1:
+    aj.display(a,b)
+else:
+    aj.subtract(a,b)
+# aj.Multiply(a,b)
+
+
+
+
+
+
+
+
+
+
+
